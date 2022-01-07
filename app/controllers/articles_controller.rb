fabc24596ca6a8376c1d95955b887f9a0043dacc
sk8marlon <marlonsgb@hotmail.com>
2022-01-07 14:12:12 -0300

    erros messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,18 +5,21 @@ def show
 end
 
 def index
-  @articles = Article.all
+  @article = Article.all
 end
 
 def new
-
+  @article = Article.new
 end
 
 def create
   @article = Article.new(params.require(:article).permit(:title, :description))
-  @article.save
-  redirect_to article_path(@article)
-
+  if @article.save
+   flash[:notice] = "Article was created sucessfully."
+   redirect_to @article
+  else
+   render 'new'
+  end
 end
 
 end
